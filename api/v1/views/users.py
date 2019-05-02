699638b33f3b5f9278bec17f460f9affe0fbb635
stak21 <stakashima21@gmail.com>
2019-05-02 17:54:58 +0000

    View for app, User objects

---
+++ api/v1/views/users.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" View for User objects """
+from api.v1.views import app_views
+from flask import Flask, request, jsonify, abort
+from models import storage
+from models.user import User 
+
+import json
+
+
+@app_views.route("/users", methods=["GET", "POST"],
+                  strict_slashes=False)
+def user_list():
+    """ GET: Render a list of users 
+        POST: creates a new user
+    """
+    if request.method == "POST":
+        new_dict = request.get_json(silent=True)
+        if not new_dict:
+            return jsonify({"error": "Not a JSON"}), 400
+        if "name" not in new_dict:
+            return jsonify({"error": "Missing name"}), 400
+        new_user = User(**new_dict)
+        storage.new(new_user)
+        storage.save()
+        storage.close()
+        return jsonify(new_user.to_dict()), 201
+    users = storage.all(User)
+    user_list = [user.to_dict() for user in users.values()] 
+    return jsonify(user_list)
+
+
+@app_views.route("/users/<user_id>", methods=["GET", "PUT", "DELETE"],
+                 strict_slashes=False)
+def detail_user(user_id):
+    """ Work on a specific user """
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    if request.method == "DELETE":
+        storage.delete(user)
+        storage.save()
+        storage.close()
+        return jsonify({})
+    if request.method == "PUT":
+        new_dict = request.get_json(silent=True)
+        if not new_dict:
+            return jsonify({"error": "Not a JSON"}), 400
+        for k, v in new_dict.items():
+            if k not in ["id", "updated_at", "created_at"]:
+                setattr(user, k, v)
+        user.save()
+    return jsonify(user.to_dict())
